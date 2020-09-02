609cf2f0e0e362da8f54348b31984787a44f285d
robert <>
2020-09-02 23:43:44 +0800

    异步编程可视化寻路算法

--- Week_02/map.js
+++ Week_02/map.js
@@ -36,6 +36,12 @@ document.addEventListener("mouseup", () =>  mousedown = false);
 //因为要使用右键，所以要把contextmenu事件preventDefault掉，不然会弹出菜单
 document.addEventListener("contextmenu", e => e.preventDefault()); 
 
+function sleep(milliseconds) {
+    return new Promise(function (resolve){
+        setTimeout(resolve, milliseconds);
+    })
+}
+
 /**
  * 广度优先搜索
  * @param {*} map 地图
@@ -44,11 +50,11 @@ document.addEventListener("contextmenu", e => e.preventDefault());
  * 
  * 如果把这里的push和shift改为push和pop,queue就变成了栈，整个算法就变成了深度优先搜索
  */
-function path(map, start, end) {
+async function path(map, start, end) {
     //这个集合，是所有搜索算法的灵魂，所有的搜索算法的差异部分其实完全就在于这个queue集合里面
     let queue = [start];
 
-    function insert(x, y){
+    async function insert(x, y){
 
         // 等于100应该也是可以的
         if (x < 0 || x >=100 || y < 0 || y >= 100) {
@@ -57,6 +63,8 @@ function path(map, start, end) {
         if (map[y*100 + x]) {
             return;
         }
+        await sleep(10);
+        container.children[y*100 + x].style.backgroundColor = "lightgreen";
         //遍历过的标记为2
         map[y*100 + x] = 2;
         queue.push([x, y]);
@@ -65,15 +73,15 @@ function path(map, start, end) {
     while (queue.length > 0) {
         // 如果找到了end，则退出
         let [x, y] = queue.shift();
-        console.log(x, y)
+        // console.log(x, y)
         if (x === end[0] && y === end[1]) {
             return true;
         }
         // 把上下左右的都加入queue
-        insert(x+1, y);
-        insert(x-1, y);
-        insert(x, y+1);
-        insert(x, y-1);
+       await insert(x+1, y);
+       await insert(x-1, y);
+       await insert(x, y+1);
+       await insert(x, y-1);
 
     }
     return false;
