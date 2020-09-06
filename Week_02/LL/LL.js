dd45f2deefc23410b2abe141f90ff7637b4aa92a
robert <>
2020-09-06 18:14:25 +0800

    week02 09课 LL词法分析:返回{type:'',valve:''}结构

--- Week_02/LL/LL.js
+++ Week_02/LL/LL.js
@@ -4,20 +4,39 @@ var regexp = /([0-9\.]+)|([ \t]+)|([\r\t]+)|(\*)|(\/)|(\+)|(\-)/g;
 
 var dictionary = ["Number", "Whitespace", "LineTerminator", "*", "/", "+", "-"];
 
-function tokenize(source) {
+function* tokenize(source) {
     var result = null;
+    var lastIndex = 0;
     while(true) {
+        lastIndex = regexp.lastIndex;
         result = regexp.exec(source);
 
         if (!result) break;
 
+        // 如果匹配的长度大于返回值得长度，说明里面有我们不认识的字符或格式，break
+        if (regexp.lastIndex - lastIndex > result[0].length) 
+           break;
+
+        let token = {
+            type: null,
+            value: null,
+        }
+
         for(let i = 1; i <= dictionary.length; i++) {
             if (result[i]) {
-                console.log(dictionary[i-1]);
+                token.type = dictionary[i-1]
             }
         }
-        console.log("result", result);
+        token.value = result[0];
+        // todo 
+        // 当我们要返回一个序列的时候，可以用yield
+        yield token;
+    }
+    yield {
+        type: "EOF"
     }
 }
 
-tokenize("1024 + 10 * 25");
+for (let token  of tokenize("1024 + 10 * 25")) {
+    console.log(token);
+}
