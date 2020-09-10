ace64063be04fa2febc0b59907c5c0f2e0056b6b
robert <>
2020-09-11 00:16:19 +0800

    week03 lesson06

--- Week_03/src/proxy/proxy.js
+++ Week_03/src/proxy/proxy.js
@@ -5,7 +5,26 @@ let  object = {
 
 // 对 po设置属性就会触发操作，对不存在的属性设置值也会触发，对object操作不会
 let po = new Proxy(object, {
-    set(obj, pro, val){
-        console.log(obj, pro, val);
+    set(obj, prop, val){
+        console.log(obj, prop, val);
     }
 })
+
+function reactive(object) {
+    return new Proxy(object, {
+                set(obj, prop, val){
+                    obj[prop] = val;
+                    console.log(obj, prop, val);
+                    return obj[prop];
+                },
+                get(obj, prop){
+                    console.log("proxy get",obj[prop])
+                    return obj[prop];
+                }
+    })
+}
+let  object2 = {
+    a: 1,
+    b: 2,
+}
+let po2 = reactive(object2);
