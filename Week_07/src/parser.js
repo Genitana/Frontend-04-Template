ea0ada330759331caa037921a55f4093d356b875
robert <>
2020-10-18 17:27:03 +0800

    week07 lesson 8~12

--- Week_07/src/parser.js
+++ Week_07/src/parser.js
@@ -1,9 +1,108 @@
+const css  = require("css");
+
 const EOF  = Symbol("EOF"); // EOF: End of File
 
 let currentToken = null;
 let currentAttribute = null;
 let stack = [{type: "document", children:[]}];
 let currentTextNode = null;
+
+// 加入一个新的函数，addCSSRules，这里我们把css规则暂存到一个数组里
+let rules = [];
+
+function addCSSRules(text) {
+    var ast = css.parse(text);    // 用css包提供的parse函数把text解析成AST
+    rules.push(...ast.stylesheet.rules);
+}
+
+/**
+ * 元素和选择器是否匹配   
+ * 这里只考虑选择器都是简单选择器：.a 、 #a  、 div  
+ * 1,以点开头的class选择器：.a  
+ * 2，以#号开头的ID选择器：#a  
+ * 3，tagName选择器：div
+ * @param {*} element 元素
+ * @param {*} selector 选择器
+ */
+function match(element, selector){
+    //! element.attributes判断是否是一个文本节点，如果是文本节点，那根本不用看匹不匹配，直接返回false
+    if(!selector || ! element.attributes)
+        return false;
+
+    if(selector.charAt(0) === "#"){
+        var attr = element.attributes.filter(e => e.name === "id")[0];
+        if(attr && attr.value === selector.replace("#", "")){
+            return true;
+        }
+    }else if(selector.charAt(0) === "."){
+        var attr = element.attributes.filter(e => e.name === "class")[0];
+        if(attr && attr.value === selector.replace(".", "")){
+            return true;
+        }
+    }else {
+        if(element.tagName === selector){
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * 计算CSS
+ */
+function computeCSS(element){
+    //获取父元素序列  ( slice()是避免后面stack被操作了，现在先复制一份)
+    var elements = stack.slice().reverse();
+
+    if(!element.computedStyle){
+        element.computedStyle = {};
+
+        for(let rule of rules){
+            //对复杂选择器根据空格拆分 
+            // 为了跟elements顺序一致，把选择器也reserve
+            var selectorParts = rule.selectors[0].split(" ").reverse();
+
+            // 当前元素跟selectorParts[0]是否匹配
+            if(!match(element, selectorParts[0])){
+                continue;
+            }
+
+            /**
+             * 双循环选择器和元素的父元素，去找到他们是否能够进行匹配
+             * 这是两个数组同时进行的循环，这块的算法是整个选择器匹配算法的里面的一个小小的难点
+             *  elements是当前元素element的父级数组，selectorParts是当前CSS匹配规则数组
+             */
+
+            let matched = false;
+
+            /**
+             * 用当前元素的父级elements，挨个去匹配CSS选择器规则，如果能匹配上，说明CSS规则匹配上了这个元素
+             * 例如：第一个img元素的父级elements：[div元素, body元素, html元素, document元素]
+             *      第一个img元素的选择器selectorParts：['#myid','div','body']
+             *       如果能匹配上selectorParts的所有规则，则说明当前CSS规则是适用于当前element元素的
+             */
+
+            var j = 1; //j：当前选择器的位置
+            for(var i = 0; i < elements.length; i++){  
+                //elements是当前元素element的父级
+                if(match(elements[i], selectorParts[j])){
+                    // 一旦元素能够匹配一个选择器，就让j自增
+                    j++;
+                }
+            }
+            //是否所有的选择器都被匹配到了
+            if(j >= selectorParts.length) {
+                //如果都匹配到了，则匹配成功
+                matched = true;
+            }
+
+            if(matched){
+                // 如果匹配到，把rules里面的所有css属性应用到这个元素上
+                console.log("匹配成功，element:", element.tagName, ", matched rule:", rule.selectors[0]);
+            }
+        }
+    }
+}
 
 /**
  * 用token构建DOM树
@@ -30,9 +129,13 @@ function emit(token) {
                 });
             }
         }
+        
+        console.log('computeCSS:', element.tagName)
+        computeCSS(element);
+
         top.children.push(element);
         element.parent = top;
-        // todo 
+        
         if(!token.isSelfClosing){
             // 不是自封闭标签，就push进stack。(push进stack的目的是干啥？就为了配对？)
             stack.push(element);
@@ -43,6 +146,10 @@ function emit(token) {
             //不配对就抛错
             throw new Error("Tag start end doesn't match!");
         }else{
+            /***** 遇到style标签，执行添加css规则的操作  (暂时不处理link标签的情况)  *****/
+            if(top.tagName === "style"){
+                addCSSRules(top.children[0].content);
+            }
             stack.pop();
         }
         currentTextNode = null;
@@ -57,7 +164,7 @@ function emit(token) {
             top.children.push(currentTextNode);
         }
         // 多个文本节点要合并
-        currentTextNode += token.content;
+        currentTextNode.content += token.content;
     }
 }
 /**
@@ -95,6 +202,10 @@ function tagOpen(c) {
         };
         return tagName(c);   // "<"后面跟着字母的，要么是开始标签，要么是自封闭标签
     }else {
+        emit({
+            type: "text",
+            content: c
+        });
         return;
     }
 }
@@ -130,6 +241,7 @@ function  tagName(c) {
         emit(currentToken);
         return data;             // 如果是“>”，说明标签结束了，开始解析下一个标签
     }else {
+        currentToken.tagName += c;
         return tagName;
     }
 }
@@ -200,7 +312,7 @@ function beforeAttributeValue(c) {
     }else if("\'" === c){
         return singleQuotedAttributeValue; //单引号
     }else if(">" === c){
-
+        return data;
     }else {
         return unquotedAttributeValue(c);
     }
@@ -231,7 +343,7 @@ function singleQuotedAttributeValue(c) {
     }else if(EOF === c){
 
     }else{
-        currentAttribute += c;
+        currentAttribute.value += c;
         return singleQuotedAttributeValue;
     }
 }
@@ -247,6 +359,8 @@ function afterQuotedAttributeValue(c) {
     }else if(EOF === c){
 
     }else{
+        //todo 感觉这一步要做个判断，从双引号来的，return doubleQuotedAttributeValue；
+        // 从单引号来的，要 return singleQuotedAttributeValue;
         currentAttribute.value += c;
         return doubleQuotedAttributeValue;
     }
@@ -275,7 +389,8 @@ function unquotedAttributeValue(c) {
 }
 function selfClosingStartTag(c) {
     if (">" === c) {
-        currentToken.selfClosing = true;
+        currentToken.isSelfClosing = true;
+        emit(currentToken);
         return data;
     }else if ("EOF" === c) {
 
@@ -285,7 +400,6 @@ function selfClosingStartTag(c) {
 }
 module.exports.parseHTML = function parseHTML(html) {
     let state = data;
-    let lastState = null;
     for(let c of html){
         if(typeof state !== "function"){
             console.error("qqqq");
@@ -295,9 +409,9 @@ module.exports.parseHTML = function parseHTML(html) {
             state = state(c);
         } catch (error) {
             console.error(error);
-            return;
         }
     }
     state = state(EOF);
     console.log(stack[0]);
+    return stack[0];
 }
