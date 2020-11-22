ed369bd22c25082c865ed1eb6f15664f220c004b
robert <>
2020-11-22 18:02:45 +0800

    week12 lesson 05~06

--- Week_12/jsx/src/main.js
+++ Week_12/jsx/src/main.js
@@ -10,13 +10,65 @@ class Carousel extends Component{
         this.attributes[name] = value;
     }
     render() {
-        console.log(555);
         this.root = document.createElement("div");
+        this.root.classList.add("carousel");
         for (let record of this.attributes.src) {
-            let child = document.createElement("img");
-            child.src = record;
+            let child = document.createElement("div");
+            child.style.backgroundImage = `url(${record})`;
             this.root.appendChild(child);
         }
+
+        let position = 0;  // 记录当前显示的是第几张图片，下次移动鼠标时，需要从当前位置挪动
+
+        this.root.addEventListener("mousedown", event => {
+            let children = this.root.children;
+            let startX = event.clientX; // mousedown的鼠标位置
+
+            let move = event => {
+                let x = event.clientX - startX; // 鼠标X轴方向上移动的距离
+
+                for (let child of children) {
+                    child.style.transition = "none";
+                    child.style.transform = `translateX(${- position * 500 + x}px)`; // css里给的图片宽度是500
+                }
+            }
+            let up = event => {
+                let x = event.clientX - startX; 
+                position = position - Math.round(x / 500);  // 图片拖够了一半，就显示下一张
+                for (let child of children) {
+                    child.style.transition = "";
+                    child.style.transform = `translateX(${- position * 500}px)`;
+                }
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+            }
+            // 在document上监听mouse up和down，因为鼠标可能会移出图片
+            document.addEventListener("mousemove", move) 
+            document.addEventListener("mouseup", up);
+        })
+        /*
+        let currentIndex = 0;
+        setInterval(() => {
+            let children = this.root.children;
+
+            let nextIndex = (currentIndex + 1) % children.length;
+
+            let current = children[currentIndex];
+            let next = children[nextIndex];
+
+            next.style.transition = "none";
+            next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+            setTimeout(() => {
+                next.style.transition = "";
+                current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+                next.style.transform = `translateX(${- nextIndex * 100}%)`;
+
+                currentIndex = nextIndex;
+            }, 16); // 16ms是浏览器里一帧的时间
+
+        },3000); */
+
         return this.root;
     }
     mountTo(parent) {
