b6ada7d7411edd0348b687cfe7355f615636cc28
robert <>
2020-12-05 17:59:16 +0800

    week14 lesson 03 实现手势的逻辑

--- Week_14/src/gesture.js
+++ Week_14/src/gesture.js
@@ -48,18 +48,68 @@ element.addEventListener("touchcancel", event => {
     }
 });
 
+/**
+ * 从 start开始，如果超过0.5s就是press；如果移动距离超过10px就是pan；如果都没有就是tab
+ */
+
+let handler;
+let startX, startY;
+let isPan = false, isTap = true, isPress = false;
+
 let start = (point) => {    
-    console.log("start", point.clientX, point.clientY);
+
+    startX = point.clientX;
+    startY = point.clientY;
+
+    isTap = true;
+    isPan = false;
+    isPress = false;
+
+    handler = setTimeout(() => {
+        isTap = false;
+        isPan = false;
+        isPress = true;
+        
+        handler = null;
+        console.log("press");
+    }, 500);                   // 0.5s时间判断是否是press
 }
+
 let move = (point) => {
-    console.log("move", point.clientX, point.clientY);
+    let dx = point.clientX - startX, dy = point.clientY - startY; //x,y轴移动的距离
+
+    // 是否移动了10px
+    if(!isPan && dx ** 2 + dy ** 2 > 100) {
+        isTap = false;
+        isPan = true;            // 移动了10px再移回去也是要触发pan事件，设置isPan是为了这个，只要大于10，就置为true
+        isPress = false;
+        console.log("panstart");
+        clearTimeout(handler);
+    }
     
+    if(isPan) {
+        console.log(dx,dy);  
+        console.log("pan");
+    }
+    // console.log("move",point.clientX, point.clientY);
 }
+
 let end = (point) => {
-    console.log("end", point.clientX, point.clientY);
+    if (isTap) {
+        console.log("tap");
+        clearTimeout(handler);
+    }
+    if(isPan) {
+        console.log("panEnd");
+    }
+    if(isPress) {
+        console.log("pressEnd");
+    }
+    // console.log("end", point.clientX, point.clientY);
     
 }
 let cancel = (point) => {
+    clearTimeout(handler);
     console.log("cancel", point.clientX, point.clientY);
 
 }
