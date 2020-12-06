48dd22e908bd283c3e8b7aec1a40e047041550e4
robert <>
2020-12-06 15:43:10 +0800

    week14 lesson06 实现一个flick事件

--- Week_14/src/gesture.js
+++ Week_14/src/gesture.js
@@ -116,6 +116,11 @@ let start = (point, context) => {
 
     context.startX = point.clientX;
     context.startY = point.clientY;
+    context.points = [{
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    }];
 
     context.isTap = true;
     context.isPan = false;
@@ -147,6 +152,14 @@ let move = (point, context) => {
         console.log(dx,dy);  
         console.log("pan");
     }
+
+    context.points = context.points.filter(point => Date.now() - point.t < 500);  // 过滤大于0.5s的
+
+    context.points.push({  // 储存一系列的点后面计算速度
+        t: Date.now(),
+        x: point.clientX,
+        y: point.clientY
+    });
     // console.log("move",point.clientX, point.clientY);
 }
 
@@ -161,9 +174,28 @@ let end = (point, context) => {
     if(context.isPress) {
         console.log("pressEnd");
     }
-    // console.log("end", point.clientX, point.clientY);
-    
+
+    context.points = context.points.filter(point => Date.now() - point.t < 500);
+
+    let d, v;
+    if (!context.points.length) {
+        v = 0;
+    }else {
+        d = Math.sqrt((point.clientX - context.points[0].x) ** 2 + 
+                (point.clientY - context.points[0].y) ** 2) ;  
+        v = d / (Date.now() - context.points[0].t);
+    }
+
+    if (v > 1.5) {
+        context.isFlick = true;
+        console.log("flick");
+    }else {
+        context.isFlick = false;
+    }
+
+    console.log("v:", v);
 }
+
 let cancel = (point, context) => {
     clearTimeout(context.handler);
     console.log("cancel", point.clientX, point.clientY);
